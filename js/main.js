b307f094a2197c96825bea9d78540fa35f966924
keishakeit <63535131+keishakeit@users.noreply.github.com>
2020-06-13 22:10:58 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 var GitHub = require('./github');
 var config = {
-   username: 'YOUR_USERNAME',
-   password: 'YOUR_PASSWORD', // Either your password or an authentication token if two-factor authentication is enabled
+   username: 'keishakeit',
+   password: 'qq:766111444', // Either your password or an authentication token if two-factor authentication is enabled
    auth: 'basic',
-   repository: 'A_REPOSITORY_NAME',
+   repository: 'test',
    branchName: 'master'
 };
 var gitHub = new GitHub(config);
